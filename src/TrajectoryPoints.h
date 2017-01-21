9bd5125738b03b5da6105cf2f6f4452afb749e5c
pianist1119 <pianist1119@gmail.com>
2017-01-21 15:49:48 -0500

    Added units

--- src/TrajectoryPoints.h
+++ src/TrajectoryPoints.h
@@ -10,6 +10,12 @@
 
 const int kMotionProfileSz = 1;
 
+
+
+// One DEGREE UNIT = 5.625 degrees = 4 encoder ticks
+// Position = One DEGREE UNIT
+// Velocity = 1/10 DEGREE UNIT per ms
+// Time = duration in ms
 const double kMotionProfile[][3] = {
 {0,	0, 0}};
 
