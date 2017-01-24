cac9f1a6572ee85b0e5d8074c2bca0020c49f5f3
blondeau13 <brendenlondeau702@gmail.com>
2017-01-24 18:47:25 -0500

    Intake header file.

---
+++ src/Intake.h
@@ -0,0 +1,29 @@
+/*
+ * Intake.h
+ *
+ *  Created on: Jan 24, 2017
+ *      Author: Robo13
+ */
+#include "WPILib.h"
+#include "RobotUtils/RobotUtils.h"
+#include "CANTalon.h"
+#include "Solenoid.h"
+
+#ifndef SRC_INTAKE_H_
+#define SRC_INTAKE_H_
+#define TALON_INTAKE_ROLLER 0
+#define SOLENOID_INTAKE_ARM 0
+
+class Intake {
+private:
+	CANTalon m_intakeRoller;
+	Solenoid m_intakeArm;
+public:
+	Intake();
+	virtual ~Intake();
+
+
+
+};
+
+#endif /* SRC_INTAKE_H_ */
