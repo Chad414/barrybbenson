9246bd13fb84c26fa09d11934ffdde6989acffa9
AngryVelociraptor <pauljohnecheck@gmail.com>
2017-01-28 11:26:57 -0500

    Add files via upload

---
+++ src/GripPipeline.cpp
@@ -0,0 +1,114 @@
+#include "GripPipeline.h"
+/**
+* Initializes a GripPipeline.
+*/
+
+namespace grip {
+
+GripPipeline::GripPipeline() {
+}
+/**
+* Runs an iteration of the Pipeline and updates outputs.
+*
+* Sources need to be set before calling this method. 
+*
+*/
+void GripPipeline::process(cv::Mat source0){
+	//Step HSV_Threshold0:
+	//input
+	cv::Mat hsvThresholdInput = source0;
+	double hsvThresholdHue[] = {69.92805755395683, 101.67235494880548};
+	double hsvThresholdSaturation[] = {144.46942446043167, 255.0};
+	double hsvThresholdValue[] = {160.52158273381295, 255.0};
+	hsvThreshold(hsvThresholdInput, hsvThresholdHue, hsvThresholdSaturation, hsvThresholdValue, this->hsvThresholdOutput);
+	//Step CV_erode0:
+	//input
+	cv::Mat cvErodeSrc = hsvThresholdOutput;
+	cv::Mat cvErodeKernel;
+	cv::Point cvErodeAnchor(-1, -1);
+	double cvErodeIterations = 1;  // default Double
+    int cvErodeBordertype = cv::BORDER_CONSTANT;
+	cv::Scalar cvErodeBordervalue(-1);
+	cvErode(cvErodeSrc, cvErodeKernel, cvErodeAnchor, cvErodeIterations, cvErodeBordertype, cvErodeBordervalue, this->cvErodeOutput);
+	//Step Find_Contours0:
+	//input
+	cv::Mat findContoursInput = cvErodeOutput;
+	bool findContoursExternalOnly = false;  // default Boolean
+	findContours(findContoursInput, findContoursExternalOnly, this->findContoursOutput);
+}
+
+/**
+ * This method is a generated setter for source0.
+ * @param source the Mat to set
+ */
+void GripPipeline::setsource0(cv::Mat &source0){
+	source0.copyTo(this->source0);
+}
+/**
+ * This method is a generated getter for the output of a HSV_Threshold.
+ * @return Mat output from HSV_Threshold.
+ */
+cv::Mat* GripPipeline::gethsvThresholdOutput(){
+	return &(this->hsvThresholdOutput);
+}
+/**
+ * This method is a generated getter for the output of a CV_erode.
+ * @return Mat output from CV_erode.
+ */
+cv::Mat* GripPipeline::getcvErodeOutput(){
+	return &(this->cvErodeOutput);
+}
+/**
+ * This method is a generated getter for the output of a Find_Contours.
+ * @return ContoursReport output from Find_Contours.
+ */
+std::vector<std::vector<cv::Point> >* GripPipeline::getfindContoursOutput(){
+	return &(this->findContoursOutput);
+}
+	/**
+	 * Segment an image based on hue, saturation, and value ranges.
+	 *
+	 * @param input The image on which to perform the HSL threshold.
+	 * @param hue The min and max hue.
+	 * @param sat The min and max saturation.
+	 * @param val The min and max value.
+	 * @param output The image in which to store the output.
+	 */
+	void GripPipeline::hsvThreshold(cv::Mat &input, double hue[], double sat[], double val[], cv::Mat &out) {
+		cv::cvtColor(input, out, cv::COLOR_BGR2HSV);
+		cv::inRange(out,cv::Scalar(hue[0], sat[0], val[0]), cv::Scalar(hue[1], sat[1], val[1]), out);
+	}
+
+	/**
+	 * Expands area of lower value in an image.
+	 * @param src the Image to erode.
+	 * @param kernel the kernel for erosion.
+	 * @param anchor the center of the kernel.
+	 * @param iterations the number of times to perform the erosion.
+	 * @param borderType pixel extrapolation method.
+	 * @param borderValue value to be used for a constant border.
+	 * @param dst Output Image.
+	 */
+	void GripPipeline::cvErode(cv::Mat &src, cv::Mat &kernel, cv::Point &anchor, double iterations, int borderType, cv::Scalar &borderValue, cv::Mat &dst) {
+		cv::erode(src, dst, kernel, anchor, (int)iterations, borderType, borderValue);
+	}
+
+	/**
+	 * Finds contours in an image.
+	 *
+	 * @param input The image to find contours in.
+	 * @param externalOnly if only external contours are to be found.
+	 * @param contours vector of contours to put contours in.
+	 */
+	void GripPipeline::findContours(cv::Mat &input, bool externalOnly, std::vector<std::vector<cv::Point> > &contours) {
+		std::vector<cv::Vec4i> hierarchy;
+		contours.clear();
+		int mode = externalOnly ? cv::RETR_EXTERNAL : cv::RETR_LIST;
+		int method = cv::CHAIN_APPROX_SIMPLE;
+		cv::findContours(input, contours, hierarchy, mode, method);
+	}
+
+
+
+} // end grip namespace
+
