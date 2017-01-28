44f12508eb8d003a8867a310134f58f68b50e9eb
Marlina <marlinabowring@hotmail.com>
2017-01-28 10:40:26 -0500

    170128 - joystick mapping

--- src/barrybbenson.cpp
+++ src/barrybbenson.cpp
@@ -1,6 +1,72 @@
 #include "WPILib.h"
 #include "RobotUtils/RobotUtils.h"
 
+/*
+	 ***** Driver Joystick Mapping
+	 *
+	 * BUTTONS
+	 * 		A -
+	 * 		B -
+	 * 		X -
+	 * 		Y -
+	 *
+	 * DPAD
+	 * 		UP -
+	 * 		DOWN -
+	 * 		LEFT -
+	 * 		RIGHT -
+	 *
+	 * JOYSTICK
+	 * 		LEFT - up/down drive
+	 * 		RIGHT - left/right drive
+	 *
+	 * BUMPERS
+	 * 		LEFT - gear shifter (hold)
+	 * 		RIGHT -
+	 *
+	 * TRIGGERS
+	 * 		LEFT - paddle out
+	 * 		RIGHT - paddle in
+	 *
+	 * OTHER
+	 * 		START -
+	 * 		BACK -
+	 *
+	 */
+
+/*
+	 ***** Operator Joystick Mapping
+	 *
+	 * BUTTONS
+	 * 		A - gear to ground
+	 * 		B - gear to place, wrist out
+	 * 		X - gear to angle off gear
+	 * 		Y - gear to package
+	 *
+	 * DPAD
+	 * 		UP - pickup out, hopper out
+	 * 		DOWN - pickup in, hopper in
+	 * 		LEFT -
+	 * 		RIGHT - hopper in
+	 *
+	 * JOYSTICK
+	 * 		LEFT - manual gear arm movement
+	 * 		RIGHT -
+	 *
+	 * BUMPERS
+	 * 		LEFT - gear out roller
+	 * 		RIGHT - gear in roller
+	 *
+	 * TRIGGERS
+	 * 		LEFT - fuel out roller
+	 * 		RIGHT - fuel in roller
+	 *
+	 * OTHER
+	 * 		START - shooter up
+	 * 		BACK -
+	 *
+	 */
+
 class barrybbenson: public HotBot {
 private:
 
