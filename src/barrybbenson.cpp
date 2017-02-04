1fff7f390d9c65a8c6ce978b46630dfc88520e7b
Riley Morris <rileymorrisgeckos@gmail.com>
2017-02-04 11:03:53 -0500

    PID at last.
    $s@g

--- src/barrybbenson.cpp
+++ src/barrybbenson.cpp
@@ -1,5 +1,16 @@
 #include "WPILib.h"
 #include "RobotUtils/RobotUtils.h"
+#include <CANTalon.h>
+
+bool continuous;
+bool shot;
+bool shooting;
+
+double setpoint;
+
+int p;
+int i;
+int d;
 
 class barrybbenson: public HotBot {
 private:
@@ -7,15 +18,17 @@ private:
 	HotJoystick* m_driver;
 	HotJoystick* m_operator;
 
-	TalonSRX * m_shooter1;
-	TalonSRX * m_shooter2;
-	TalonSRX * m_paddle;
+	CANTalon * m_shooter1;
+	CANTalon * m_shooter2;
+	CANTalon * m_paddle;
 
 	PIDController * m_shooterPID;
 
 	Encoder * m_shooterEncoder;
 
 	PowerDistributionPanel* m_pdp;
+	
+	Timer * m_shooterTime;
 
 	double speed;
 	//int timesPressed;
@@ -27,15 +40,13 @@ public:
 		m_driver = new HotJoystick(0);
 		m_operator = new HotJoystick(1);
 
-		m_shooter1 = new TalonSRX( 7 );
-		m_shooter2 = new TalonSRX( 12 );
-		m_paddle = new TalonSRX( 1 );
+		m_shooter1->SetFeedbackDevice( CANTalon::CtreMagEncoder_Relative );
 
-		m_shooterEncoder = new Encoder( 0,1 );
+		m_shooterPID = new PIDController( p, i, d, m_shooter1, m_shooterEncoder );
+		
+		m_shooterTime = new Timer();
 
-		m_shooterPID = new PIDController( 1 , 0 , 0 , m_shooter1 , m_shooterEncoder );
-
-		m_pdp = new PowerDistributionPanel(0);
+		m_pdp = new PowerDistributionPanel( 0 );
 	}
 	void RobotInit() {
 		}
@@ -53,6 +64,14 @@ public:
 		speed = 0;
 		previousAButton = false;
 		previousXButton = false;
+		m_shooterPID->SetSetpoint( 0 );
+		m_shooterPID->SetContinuous( continuous );
+
+		m_shooterPID->SetPID( 1, 0 , 0 );
+
+	    m_shooterTime->Reset();
+
+		shot = false;
 	}
 
 	void TeleopPeriodic() {
@@ -60,28 +79,49 @@ public:
 
 	 previousAButton= m_driver->ButtonA();
 	 previousXButton= m_driver->ButtonX();
-
-
 	}
 
 	void TeleopShoot() {
-
-		if (m_driver->ButtonX() ) {
-			speed = .5;
+			if (m_driver->ButtonX() ) {
+						shot = true;
+						shooting = true;
+						setpoint = 2000; // placeholder values
 			}
-		else if (m_driver->ButtonA() ) {
-			speed = -.5;
+			else if (m_driver->ButtonA() ) {
+						shot = true;
+						shooting = true;
+						setpoint = -2000; //placeholder values
 			}
-
-		else
+		if ( shot == true )
 		{
-			speed = 0.0;
-		}
+			m_shooterPID->Enable();
+			m_shooterPID->SetSetpoint( setpoint );
 
-			m_shooter1->Set( speed );
-			m_shooter2->Set( speed );
-			m_paddle->Set( speed );
+			while ( shooting == true )
+				{
+
+					if ( m_shooterPID->GetError() > 1000 )
+						{
+							m_shooterPID->SetPID( 5, 1, 2 );
+						}
+					else if ( m_shooterPID->GetError() > 500 && m_shooterPID->GetError() < 1000 )
+						{
+							m_shooterPID->SetPID( 3, 0, 1 );
+						}
+					else
+						{
+							m_shooterPID->SetPID( 1, 0, 0 );
+						}
+
+					if ( m_driver->ButtonX() )
+						{
+							shooting = false;
+						}
+				}
+					m_shooterPID->SetSetpoint( 0 );
 		}
+	}
+		
 
 
 
