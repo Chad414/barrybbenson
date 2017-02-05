cdb9d34761a9bebce19b61dc8cf2a77bfdf18119
Mark Schang <mark@schang.net>
2017-02-05 14:29:24 -0500

    temp

--- src/barrybbenson.cpp
+++ src/barrybbenson.cpp
@@ -27,11 +27,7 @@ private:
 
 public:
 	barrybbenson() {
-		m_driver = new HotJoystick(0);
-		m_operator = new HotJoystick(1);
 
-		m_PWMmotor0 = new Victor(0);
-		m_PWMmotor1 = new Victor(1);
 
 		m_CANmotor1 = new CANTalon(1);
 		m_CANmotor2 = new CANTalon(2);
@@ -75,7 +71,7 @@ public:
 	void TeleopShoot() {
 
 		potentio = m_pot->Get();
-		speed = potentio/360;
+		speed = potentio*10;
 		double slider0 = SmartDashboard::GetNumber("DB/Slider 0", 0.0);
 		double slider1 = SmartDashboard::GetNumber("DB/Slider 1", 0.0);
 		double slider2 = SmartDashboard::GetNumber("DB/Slider 2", 0.0);
@@ -83,17 +79,10 @@ public:
 
 		//m_CANmotor1->Set(slider0);
 		m_CANmotor1->Set(speed);
-		m_CANmotor2->Set(slider1);
-		m_PWMmotor0->Set(slider2);
-		m_PWMmotor1->Set(slider3);
 
 
-		SmartDashboard::PutNumber("CAN1_Encoder", m_CANmotor1->GetPosition());
-		SmartDashboard::PutNumber("Gyro", m_gyro->GetYaw());
-		SmartDashboard::PutNumber("speed", speed);
-		SmartDashboard::PutBoolean("A Button", previousAButton);
-		SmartDashboard::PutBoolean("X Button", previousXButton);
-		SmartDashboard::PutNumber("Pot", potentio);
+
+
 		SmartDashboard::PutNumber("CAN1", slider0);
 		SmartDashboard::PutNumber("CAN2", slider1);
 		SmartDashboard::PutNumber("PWM0", slider2);
