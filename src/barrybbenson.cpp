313aaefeb4dfcfd2be7b73885a517594d7edb9b8
Mark Schang <mark@schang.net>
2017-02-05 14:26:25 -0500

    Test

--- src/barrybbenson.cpp
+++ src/barrybbenson.cpp
@@ -1,50 +1,32 @@
 #include "WPILib.h"
 #include "RobotUtils/RobotUtils.h"
 #include "CANTalon.h"
-#include "AHRS.h"
+
+#define SHOOTER_P 0.25
+#define SHOOTER_I 0.15
+#define SHOOTER_D 0.0
 
 class barrybbenson: public HotBot {
 private:
-
-	HotJoystick* m_driver;
-	HotJoystick* m_operator;
-
-	Victor* m_PWMmotor0;
-	Victor* m_PWMmotor1;
 	CANTalon* m_CANmotor1;
-	CANTalon* m_CANmotor2;
-
-	AHRS *m_gyro;
-
-	PowerDistributionPanel* m_pdp;
-
-	AnalogPotentiometer* m_pot;
 
 	double speed;
-	bool previousAButton;
-	bool previousXButton;
-	double potentio;
+	double PID_P, PID_I, PID_D, PID_Current, PID_Position, PID_Speed, PID_Error, PID_Setpoint, PID_SpeedGet;
+
 
 public:
 	barrybbenson() {
-		m_driver = new HotJoystick(0);
-		m_operator = new HotJoystick(1);
+		m_CANmotor1 = new CANTalon(2);
+		m_CANmotor1->SetFeedbackDevice(CANTalon::CtreMagEncoder_Relative);
+		m_CANmotor1->SetClosedLoopOutputDirection(true);
+		m_CANmotor1->SetControlMode(CANTalon::kSpeed);
+		m_CANmotor1->SetPID(SHOOTER_P, SHOOTER_I, SHOOTER_D);
+		m_CANmotor1->Enable();
+		m_CANmotor1->SetAllowableClosedLoopErr(0);
 
-		m_PWMmotor0 = new Victor(0);
-		m_PWMmotor1 = new Victor(1);
 
-		m_CANmotor1 = new CANTalon(1);
-		m_CANmotor2 = new CANTalon(2);
 
-		m_CANmotor1->SetFeedbackDevice(CANTalon::QuadEncoder);
-		m_CANmotor1->ConfigEncoderCodesPerRev(1024);
 
-		m_pdp = new PowerDistributionPanel(0);
-
-		m_gyro = new AHRS(I2C::Port::kMXP);
-
-		AnalogInput *ai = new AnalogInput(0);
-		m_pot = new AnalogPotentiometer(ai, 360, 0);
 	}
 	void RobotInit() {
 	}
@@ -60,44 +42,44 @@ public:
 
 	void TeleopInit() {
 		speed = 0;
-		previousAButton = false;
-		previousXButton = false;
-		potentio = 0.0F;
+
 	}
 
 	void TeleopPeriodic() {
 	 TeleopShoot();
 
-	 previousAButton= m_driver->ButtonA();
-	 previousXButton= m_driver->ButtonX();
 	}
 
 	void TeleopShoot() {
 
-		potentio = m_pot->Get();
-		speed = potentio/360;
 		double slider0 = SmartDashboard::GetNumber("DB/Slider 0", 0.0);
-		double slider1 = SmartDashboard::GetNumber("DB/Slider 1", 0.0);
-		double slider2 = SmartDashboard::GetNumber("DB/Slider 2", 0.0);
-		double slider3 = SmartDashboard::GetNumber("DB/Slider 3", 0.0);
 
-		//m_CANmotor1->Set(slider0);
+		speed = -slider0;
 		m_CANmotor1->Set(speed);
-		m_CANmotor2->Set(slider1);
-		m_PWMmotor0->Set(slider2);
-		m_PWMmotor1->Set(slider3);
+
+		PID_P = m_CANmotor1->GetP();
+		PID_I = m_CANmotor1->GetI();
+		PID_D = m_CANmotor1->GetD();
+		PID_Current = m_CANmotor1->GetOutputCurrent();
+		PID_Position = m_CANmotor1->GetPosition();
+		PID_Speed = m_CANmotor1->GetSpeed();
+		PID_Error = m_CANmotor1->GetClosedLoopError();
+		PID_Setpoint = m_CANmotor1->GetSetpoint();
+		PID_SpeedGet = m_CANmotor1->Get();
 
 
-		SmartDashboard::PutNumber("CAN1_Encoder", m_CANmotor1->GetPosition());
-		SmartDashboard::PutNumber("Gyro", m_gyro->GetYaw());
-		SmartDashboard::PutNumber("speed", speed);
-		SmartDashboard::PutBoolean("A Button", previousAButton);
-		SmartDashboard::PutBoolean("X Button", previousXButton);
-		SmartDashboard::PutNumber("Pot", potentio);
-		SmartDashboard::PutNumber("CAN1", slider0);
-		SmartDashboard::PutNumber("CAN2", slider1);
-		SmartDashboard::PutNumber("PWM0", slider2);
-		SmartDashboard::PutNumber("PWM1", slider3);
+
+		SmartDashboard::PutNumber("PID_P", PID_P);
+		SmartDashboard::PutNumber("PID_I", PID_I);
+		SmartDashboard::PutNumber("PID_D", PID_D);
+		SmartDashboard::PutNumber("PID_Current", PID_Current);
+		SmartDashboard::PutNumber("PID_Position", PID_Position);
+		SmartDashboard::PutNumber("PID_Speed", PID_Speed);
+		SmartDashboard::PutNumber("PID_SetPoint/speed", speed);
+		SmartDashboard::PutNumber("PID_Error", PID_Error);
+		SmartDashboard::PutNumber("Slider0", slider0);
+		SmartDashboard::PutNumber("PID_Setpoint", PID_Setpoint);
+		SmartDashboard::PutNumber("PID_SpeedGet", PID_Setpoint);
 	}
 
 	void TestPeriodic() {
