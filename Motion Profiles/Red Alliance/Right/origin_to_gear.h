a0861c565afb6ddf0f1fcc13237196fbd3ac851e
Paul Johnecheck <pauljohnecheck@gmail.com>
2017-02-18 14:52:56 -0500

    new Branch for profiles

---
+++ Motion Profiles/Red Alliance/Right/origin_to_gear.h
@@ -0,0 +1,5 @@
+#ifndef _motionprofiles_
+#define_motionprofiles_
+std::vector<std::vector<double>> profileLeft()
+std::vector<std::vector<double>> profileRight()
+#endif
--- src/tank.h
+++
@@ -1,8 +0,0 @@
-#ifndef PATHFINDER_MOD_TANK_H_DEF
-#define PATHFINDER_MOD_TANK_H_DEF
-
-#include "lib.h"
-
-CAPI void pathfinder_modify_tank(Segment *original, int length, Segment *left, Segment *right, double wheelbase_width);
-
-#endif
