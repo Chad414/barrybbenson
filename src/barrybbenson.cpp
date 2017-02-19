06acfa3f53df1ee918cd5ef40874d2a80f004cca
Mark Schang <mark@schang.net>
2017-02-18 21:32:11 -0500

    test

--- src/barrybbenson.cpp
+++ src/barrybbenson.cpp
@@ -2,40 +2,81 @@
 #include "RobotUtils/RobotUtils.h"
 #include "CANTalon.h"
 
-#define SHOOTER_P 0.02
-#define SHOOTER_I 0.0001
-#define SHOOTER_D 0.0
-#define SHOOTER_FF 0.03325
+#define L_P 0.09
+#define L_I 0.0
+#define L_D 0.0
+#define L_FF 0.784
+
+#define R_P 0.09
+#define R_I 0.0
+#define R_D 0.0
+#define R_FF 0.774
 
 
 class barrybbenson: public HotBot {
 private:
-	CANTalon* m_CANmotor1;
-	AnalogPotentiometer* m_pot;
+	CANTalon* m_LF;
+	CANTalon* m_LR;
+	CANTalon* m_RF;
+	CANTalon* m_RR;
 
-	double speed_rpm, error_rpm;
-	double PID_P, PID_I, PID_D, PID_FF, PID_Current, PID_Position, PID_Speed, PID_Error, PID_Setpoint, PID_SpeedGet;
-	double potentio;
+	double L_error_rpm, R_error_rpm, speed_pctVbus;
+	double L_PID_P, L_PID_I, L_PID_D, L_PID_FF, L_PID_Position, L_PID_Speed, L_PID_Error, L_PID_Setpoint;
+	double R_PID_P, R_PID_I, R_PID_D, R_PID_FF, R_PID_Position, R_PID_Speed, R_PID_Error, R_PID_Setpoint;
+	double LF_PID_Current, LR_PID_Current, RF_PID_Current, RR_PID_Current;
 
 public:
 	barrybbenson() {
-		m_CANmotor1 = new CANTalon(2);
-		m_CANmotor1->SetFeedbackDevice(CANTalon::CtreMagEncoder_Relative);
-		m_CANmotor1->SetClosedLoopOutputDirection(true);
-		m_CANmotor1->SetControlMode(CANTalon::kSpeed);
+		/* Left Front Motor */
+		m_LF = new CANTalon(11);
+		m_LF->SetFeedbackDevice(CANTalon::QuadEncoder);
+		m_LF->SetControlMode(CANTalon::kSpeed);
+		m_LF->ConfigEncoderCodesPerRev(256*1.4);
+		m_LR->SetClosedLoopOutputDirection(false);
+		m_LF->SetPID(L_P, L_I, L_D);
+		m_LF->SetF(L_FF);
+		m_LF->Enable();
+		m_LF->SetAllowableClosedLoopErr(0);
+		m_LF->SetSensorDirection(true);
+		m_LF->ConfigNominalOutputVoltage(+0.0F, -0.0F);
+		m_LF->ConfigPeakOutputVoltage(+12.0F, -12.0F);
+		m_LF->SetPosition(0.0);
+		//m_CANmotor1->SetCloseLoopRampRate(65);
 
-		m_CANmotor1->SetPID(SHOOTER_P, SHOOTER_I, SHOOTER_D);
-		m_CANmotor1->SetF(SHOOTER_FF);
-		m_CANmotor1->Enable();
-		m_CANmotor1->SetAllowableClosedLoopErr(0);
-		m_CANmotor1->SetSensorDirection(false);
-		m_CANmotor1->SetClosedLoopOutputDirection(true);
-		m_CANmotor1->ConfigNominalOutputVoltage(+0.0F, -0.0F);
-		m_CANmotor1->ConfigPeakOutputVoltage(+12.0F, -12.0F);
-		m_CANmotor1->SetCloseLoopRampRate(65);
+		/* Left Rear Motor */
+		m_LR = new CANTalon(12);
+		m_LR->SetFeedbackDevice(CANTalon::QuadEncoder);
+		m_LR->SetClosedLoopOutputDirection(false);
+		m_LR->SetControlMode(CANTalon::kFollower);
+		m_LR->Set(11);
 
-		AnalogInput *ai = new AnalogInput(0);
-		m_pot = new AnalogPotentiometer(ai, 360, 0);
+
+
+
+
+		/* Right Front Motor */
+		m_RF = new CANTalon(21);
+		m_RF->SetFeedbackDevice(CANTalon::QuadEncoder);
+		m_RF->SetControlMode(CANTalon::kSpeed);
+		m_RF->SetInverted(true);
+		m_RF->ConfigEncoderCodesPerRev(256*1.4);
+		m_LR->SetClosedLoopOutputDirection(false);
+		m_RF->SetPID(R_P, R_I, R_D);
+		m_RF->SetF(R_FF);
+		m_RF->Enable();
+		m_RF->SetAllowableClosedLoopErr(0);
+		m_RF->SetSensorDirection(false);
+		m_RF->ConfigNominalOutputVoltage(+0.0F, -0.0F);
+		m_RF->ConfigPeakOutputVoltage(+12.0F, -12.0F);
+		m_RF->SetPosition(0.0);
+		//m_CANmotor1->SetCloseLoopRampRate(65);
+
+		/* Right Rear Motor */
+		m_RR = new CANTalon(20);
+		m_RR->SetFeedbackDevice(CANTalon::QuadEncoder);
+		m_RR->SetControlMode(CANTalon::kFollower);
+		m_RR->SetClosedLoopOutputDirection(false);
+		m_RR->Set(21);
 
 
 	}
@@ -52,8 +93,7 @@ public:
 	}
 
 	void TeleopInit() {
-		speed_rpm = 0.0;
-		potentio = 0.0F;
+
 	}
 
 	void TeleopPeriodic() {
@@ -62,34 +102,61 @@ public:
 	}
 
 	void TeleopShoot() {
-		potentio = m_pot->Get();
-		speed_rpm = potentio*10;  /* speed in rpm */
 
-		m_CANmotor1->Set(speed_rpm);
+		speed_pctVbus = 1.0;
 
-		PID_P = m_CANmotor1->GetP();
-		PID_I = m_CANmotor1->GetI();
-		PID_D = m_CANmotor1->GetD();
-		PID_FF = m_CANmotor1->GetF();
-		PID_Current = m_CANmotor1->GetOutputCurrent();
-		PID_Position = m_CANmotor1->GetPosition();
-		PID_Speed = m_CANmotor1->GetSpeed();
-		PID_Error = m_CANmotor1->GetClosedLoopError();  /* Closed loop error is is is Native Units per 100ms for Mag Encoder */
-		error_rpm = (PID_Error/4096.0)*600.0;           /* Formula for Mag Encoder to convert Native Units per 100ms to rpm  */
-		PID_Setpoint = m_CANmotor1->GetSetpoint();
+		m_RF->Set(200);
+		m_LF->Set(200);
 
-		SmartDashboard::PutNumber("PID_P", PID_P);
-		SmartDashboard::PutNumber("PID_I", PID_I);
-		SmartDashboard::PutNumber("PID_D", PID_D);
-		SmartDashboard::PutNumber("PID_FF", PID_FF);
-		SmartDashboard::PutNumber("PID_Current", PID_Current);
-		SmartDashboard::PutNumber("PID_Position", PID_Position);
-		SmartDashboard::PutNumber("PID_Speed", PID_Speed);
-		SmartDashboard::PutNumber("PID_Error", PID_Error);
-		SmartDashboard::PutNumber("PID_Setpoint", PID_Setpoint);
-		SmartDashboard::PutNumber("potentio", potentio);
-		SmartDashboard::PutNumber("speed_rpm", speed_rpm);
-		SmartDashboard::PutNumber("error_rpm", error_rpm);
+		L_PID_P = m_LF->GetP();
+		L_PID_I = m_LF->GetI();
+		L_PID_D = m_LF->GetD();
+		L_PID_FF = m_LF->GetF();
+		L_PID_Position = m_LF->GetPosition();
+		L_PID_Speed = m_LF->GetSpeed();
+		L_PID_Error = m_LF->GetClosedLoopError();           /* Closed loop error is is is Native Units per 100ms for Mag Encoder */
+		L_error_rpm = (L_PID_Error/4096.0)*600.0;           /* Formula for Mag Encoder to convert Native Units per 100ms to rpm  */
+		L_PID_Setpoint = m_LF->GetSetpoint();
+
+		R_PID_P = m_RF->GetP();
+		R_PID_I = m_RF->GetI();
+		R_PID_D = m_RF->GetD();
+		R_PID_FF = m_RF->GetF();
+		R_PID_Position = m_RF->GetPosition();
+		R_PID_Speed = m_RF->GetSpeed();
+		R_PID_Error = m_RF->GetClosedLoopError();           /* Closed loop error is is is Native Units per 100ms for Mag Encoder */
+		R_error_rpm = (R_PID_Error/4096.0)*600.0;           /* Formula for Mag Encoder to convert Native Units per 100ms to rpm  */
+		R_PID_Setpoint = m_RF->GetSetpoint();
+
+		LF_PID_Current = m_LF->GetOutputCurrent();
+		LR_PID_Current = m_LR->GetOutputCurrent();
+		RF_PID_Current = m_RF->GetOutputCurrent();
+		RR_PID_Current = m_RR->GetOutputCurrent();
+
+		SmartDashboard::PutNumber("L_PID_P", L_PID_P);
+		SmartDashboard::PutNumber("L_PID_I", L_PID_I);
+		SmartDashboard::PutNumber("L_PID_D", L_PID_D);
+		SmartDashboard::PutNumber("L_PID_FF", L_PID_FF);
+		SmartDashboard::PutNumber("L_PID_Position", L_PID_Position);
+		SmartDashboard::PutNumber("L_PID_Speed", L_PID_Speed);
+		SmartDashboard::PutNumber("L_PID_Error", L_PID_Error);
+		SmartDashboard::PutNumber("L_PID_Setpoint", L_PID_Setpoint);
+		SmartDashboard::PutNumber("L_error_rpm", L_error_rpm);
+
+		SmartDashboard::PutNumber("R_PID_P", R_PID_P);
+		SmartDashboard::PutNumber("R_PID_I", R_PID_I);
+		SmartDashboard::PutNumber("R_PID_D", R_PID_D);
+		SmartDashboard::PutNumber("R_PID_FF", R_PID_FF);
+		SmartDashboard::PutNumber("R_PID_Position", R_PID_Position);
+		SmartDashboard::PutNumber("R_PID_Speed", R_PID_Speed);
+		SmartDashboard::PutNumber("R_PID_Error", R_PID_Error);
+		SmartDashboard::PutNumber("R_PID_Setpoint", R_PID_Setpoint);
+		SmartDashboard::PutNumber("R_error_rpm", R_error_rpm);
+
+		SmartDashboard::PutNumber("LF_PID_Current", LF_PID_Current);
+		SmartDashboard::PutNumber("LR_PID_Current", LR_PID_Current);
+		SmartDashboard::PutNumber("RF_PID_Current", RF_PID_Current);
+		SmartDashboard::PutNumber("RR_PID_Current", RR_PID_Current);
 	}
 
 	void TestPeriodic() {
